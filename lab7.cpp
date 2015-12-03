ca18348aa441bfd85a18d01edb9525e2103aa051
Daniel Handojo <dhand002@indigo-02.cs.ucr.edu>
2015-12-02 16:06:54 -0800

    Finished FIFO! Made simulate_cache its own function

--- lab7.cpp
+++ lab7.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <vector>
 #include <list>
 
 using namespace std;
 
-int log2(int num);
-
+int log_2(int num);
+double simulate_cache(int cache, int asso, int rp, vector<long long>& addresses);
 struct block {
     bool valid;
     long long tag;
@@ -15,11 +16,36 @@ struct block {
         valid = false;
         tag = 0;
     }
+
+    block(bool v, long long l) {
+        valid = v;
+        tag = l;
+    }
 };
+
+// Constants
+const long TOTAL = 1012176; // file size
+const int BLOCK_BITS = 4;   // block offset, 16 elements
+const int BLOCK_ELEMS = 16; // for cache size / elem per block
+long long BITMASK[8] = {    // to get tag for different size
+    0x0000000000000007,
+    0x000000000000000F,
+    0x000000000000001F,
+    0x000000000000003F,     // 6 bits (direct map 1024)
+    0x000000000000007F,     // 7 bits (direct map 2048)
+    0x00000000000000FF,     // 8 bits (direct map 4096)
+    0x00000000000001FF,     // 9 bits (direct map 8192)
+    0x00000000000003FF};    // 10 bits (direct map 16384)
+int CACHE_SIZE[5] = {       // cache size = # of long long in cache
+    1024,
+    2048,
+    4096,
+    8192,
+    16384};
+int ASSOCIATIVITY[4] = {1,2,4,8};
 
 int main() {
     // Load all addresses into single vector first
-    const long TOTAL = 1012176;
     vector<long long> addresses(TOTAL);
 
     ifstream fs;
@@ -30,48 +56,53 @@ int main() {
 
     fs.close();
 
+    cout << setprecision(2) << fixed;
+    cout << "Miss ratio: " << simulate_cache(4, 3, 0, addresses) << endl;
+}
 
-    // Constants
-    const int BLOCK_BITS = 4;   // block offset, 16 elements
-    const int BLOCK_ELEMS = 16; // for cache size / elem per block
-    long long BITMASK[8];       // to get tag for different size
-    BITMASK[0] = 0x0000000000000007;
-    BITMASK[1] = 0x000000000000000F;
-    BITMASK[2] = 0x000000000000001F; 
-    BITMASK[3] = 0x000000000000003F; // 6 bits (direct map 1024)
-    BITMASK[4] = 0x000000000000007F; // 7 bits (direct map 2048)
-    BITMASK[5] = 0x00000000000000FF; // 8 bits (direct map 4096)
-    BITMASK[6] = 0x00000000000001FF; // 9 bits (direct map 8192)
-    BITMASK[7] = 0x00000000000003FF; // 10 bits (direct map 16384)
-    int CACHE_SIZE[5];          // cache size = # of long long in cache
-    CACHE_SIZE[0] = 1024;
-    CACHE_SIZE[1] = 2048;
-    CACHE_SIZE[2] = 4096;
-    CACHE_SIZE[3] = 8192;
-    CACHE_SIZE[4] = 16384;
-    int ASSOCIATIVITY[4];
-    ASSOCIATIVITY[0] = 1;
-    ASSOCIATIVITY[1] = 2;
-    ASSOCIATIVITY[2] = 4;
-    ASSOCIATIVITY[3] = 8;
-
+double simulate_cache(int ch, int asso, int rp, vector<long long>& addresses)
+{
     // Direct mapping, Size 1024, FIFO
     cout << hex;
-    int numCacheSets = (CACHE_SIZE[0] / BLOCK_ELEMS) / ASSOCIATIVITY[0];
-    int indexBits = log2(numCacheSets);
+    int numCacheSets = (CACHE_SIZE[ch] / BLOCK_ELEMS) / ASSOCIATIVITY[asso];
+    int indexBits = log_2(numCacheSets);
 
     // Cache
     vector< list<block> > cache(numCacheSets);
+    // Miss count
+    long miss = 0;
 
     for (int i = 0; i < TOTAL; i++) {   // trace file
         long long index = addresses.at(i) >> BLOCK_BITS;
-        index = index & BITMASK[3]; 
+        index = index & BITMASK[ch + 3 - asso]; // 3-0, or 4-1, for bit masks based on size and associativity etc.
         long long tag = addresses.at(i) >> (indexBits + BLOCK_BITS);
+
+        bool hit = false;
+        for (list<block>::iterator it = cache.at(index).begin(); it != cache.at(index).end(); it++) {
+            struct block b = *it;
+
+            if (b.valid && b.tag == tag) {
+                hit = true;
+                break;
+            }
+        }
+
+        if (!hit) {
+            if (!(cache.at(index).size() < ASSOCIATIVITY[asso])) // pop_back oldest/least recently used
+                cache.at(index).pop_back();
+
+            struct block insert(true, tag);
+            cache.at(index).push_front(insert); // Place new "data" in the cache
+            
+            miss++;
+        }
     }
+
+    return ((double) miss) / ((double) TOTAL) * 100;
 }
 
 // Basic log_2 function. will fail on non-power-of-two
-int log2(int num) {
+int log_2(int num) {
     if (num <= 0) return -1;
 
     int log = 0;
